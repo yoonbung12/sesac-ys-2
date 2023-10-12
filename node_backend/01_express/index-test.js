f08effe7e533969a4ee8fac75c27dd9970011351
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-10-12 17:29:35 +0900

    test express

---
+++ node_backend/01_express/index-test.js
@@ -0,0 +1,9 @@
+// ***************ㅕ**************8
+//express 사용을 위해 모듈 import
+const express = require('express'); 
+// app은 서버 객체다.. 
+const app = express();
+// port 번호설정
+const PORT = 8001; 
+
+app.set()
