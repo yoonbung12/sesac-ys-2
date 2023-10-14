4ad455dd1c774150966398df9daae714a9556a43
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-10-14 16:15:44 +0900

    promise를 async 사용해서 해결

--- node_test/js_hard/async.js
+++ node_test/js_hard/async.js
@@ -54,6 +54,40 @@
 //  -----------------------------------------------------
 
 // -----------------------promise로 해결하는 방법-------------
+// function goMart(){
+//     console.log("마트에 가서 어떤 음료를 살지 고민한다...");
+// }
+
+// let product;
+// let price;
+
+// function pickDrink(){
+//     // promise로  해결한 부분
+//     return new Promise(function(resolve, reject){
+//         setTimeout(function(){
+//             console.log("고민 끝!!");
+//             product = "제로 코크";
+//             price = 2000;
+//             resolve(true); // 이시점에 끝났다는것을 인지 할것!!!!그렇게 해서 then으로 넘어감
+//         }, 3000); //3초후
+//     });
+  
+// }
+
+
+// function pay(product, price){
+//     console.log(`상품: ${product},  가격 ${price}`);
+// }
+
+// goMart();
+// pickDrink().then(() => {
+//     pay(product, price);
+// }); 
+// pay(product, price);
+// ---------------------------------------
+
+
+// ------------promise를 async 로 바꿔봐
 function goMart(){
     console.log("마트에 가서 어떤 음료를 살지 고민한다...");
 }
@@ -61,7 +95,7 @@ function goMart(){
 let product;
 let price;
 
-function pickDrink(){
+async function pickDrink(){
     // promise로  해결한 부분
     return new Promise(function(resolve, reject){
         setTimeout(function(){
@@ -79,10 +113,18 @@ function pay(product, price){
     console.log(`상품: ${product},  가격 ${price}`);
 }
 
-goMart();
-pickDrink().then(() => {
-    pay(product, price);
-}); 
-// pay(product, price);
 
-// --------------------
+
+// goMart();
+// pickDrink().then(() => {
+//     pay(product, price);
+// }); 
+
+async function exec(){
+    goMart();
+    await pickDrink();
+    pay(product, price);
+}
+exec();
+
+// ----------------- async / await 변경 끝 ------------------
