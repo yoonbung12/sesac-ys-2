022bd94f792c90d309ed0bd164d8a3251e6c09c6
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-10-21 14:22:01 +0900

    multer: 공부

---
+++ node_backend/04_file_upload/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+// 파일 업로드 할려고 multer 가져옴
+const multer = require("multer");
+
+const path = require("path");
+// path: 퍄일 경로를 받았을때, 그 조작을 도와준다.
+// ex) 확장자 추출, 파일이름을 추출 
+console.log("hi.txt의 확장자: ", path.extname("hi.txt"));
+console.log("hi.txt의 확장자: ", path.basename("hi.txt",  path.extname("hi.txt")));
+
+const app = express();
+const PORT = 8000;
+
+// upload라는 객체 안에는 미들웨어 함수가 존재. single(), array(), filed()
+// 미들웨어: 요청과 응답 사이에 존재. (next())
+// multer 가 임의의 문자열을 생성해서 그 문자열을 파일 이름으로 만듬.
+// 심지어 확장자도 붙여주지 않음
+const upload = multer({
+    dest: "uploads/", //dest: 목적지
+});
+// 상세 설정할때
+const uploadDetail = multer({
+    //
+    storage: multer.diskStorage({
+        destination: function(req, file, done) {
+            //파일 경로 설정
+            done(null, "uploads/");
+        },
+        filename: function(req, file, done) {
+            console.log(file); // file.originalname, 미니언.webp
+            const ext = path.extname(file.originalname); // .webp
+            const basename = path.basename(file.originalname, ext ) // 미니언
+            // 미니언_1231241312.webp
+            const fileName = basename + "_" + Date.now() + ext;
+
+                //"지정할 파일 이름"
+            done(null, fileName );
+        },
+    }),
+    limits: {fileSize: 5 * 1024 * 1024}, //5MB 제한
+});
+
+// storage: 파일을 저장할 정보를 설정
+    // 1) diskStorage : 파일을 디스크에 저장하기 위한 기능을 제공하는 메소드
+    // 2) destination : 파일이 저장될 경로   
+    // 3) fileName : 파일이 저장될 이름
+
+// limits:
+    // fileSize : 파일의 최대 크기
+    
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+//  (single(), array(), fileds() 미들웨어 : 클라이언트가 보낸 요청 중에 userFile이라는 name의  파일 데이터가 있다면 )
+// 파일을 multer의 설정대로 저장해서, req.file이라는 객체를 생성해서 다음 함수에 넘겨준다.
+// (single()) : 파일 하나만 업로드할때 사용하는것 ->req.file 생성
+app.post("/upload", upload.single("userFile") , function(req, res){
+    // console.log(req.body);
+    console.log("req.file",req.file);
+    console.log("req.body", req.body);
+    res.send("파일 업로드");
+});
+
+//
+app.post("/upload/detail", uploadDetail.single("userFile"), 
+    function(req, res){
+    console.log("file detail",req.file);
+    console.log("body detail", req.body);
+    res.send("파일 업로드");
+})
+
+app.listen(PORT, function () {
+    console.log(`Sever Open: ${PORT}`);
+  });
+
