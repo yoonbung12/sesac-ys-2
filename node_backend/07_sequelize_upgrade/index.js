7b9f9eb44d813a9c377b16882103ebd2a6a01dce
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-11-05 20:33:49 +0900

    sequelize 수정

--- node_backend/07_sequelize_upgrade/index.js
+++ node_backend/07_sequelize_upgrade/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const session = require("express-session")
+const port = 8000
 
 app.set("view engine", "ejs");
 app.use("/static", express.static(__dirname + "static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  session({
+    secret: "secret key",
+    resave: false,// 모든 요청마다 session을 다시 저장할거냐??
+    saveUninitialized: true, // 클라이언트가 처음 접속할때, 세션을 한번 초기화 할건지 말건지?
+  })
+)
+
 
 const router = require("./routes");
 app.use("/", router);
+
+// 
 
 app.get("*", (req, res) => {
   res.send("접근할 수 없는 주소입니다.");
