c78701bff401c1e40aa6d0041efdbf5e8c69f595
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-12-21 13:52:43 +0900

    추가 + 수정

--- react-proj/11-socket/src/components/Chatting2.js
+++ react-proj/11-socket/src/components/Chatting2.js
@@ -31,6 +31,14 @@ export default function Chatting2() {
 
   useEffect(() => {
     // initSocketConnect();
+    socket.on("error", (res) => {
+      alert(res.msg);
+    });
+
+    socket.on("entrySuccess", (res) => {
+      setUserId(res.userId);
+    });
+
   }, []);
 
   useEffect(() => {
@@ -52,7 +60,8 @@ export default function Chatting2() {
     socket.emit("entry", { userId: userIdInput });
     // [실습 3-2] 고민해보기
     // 바로 userId에 값을 할당하지 않고
-    setUserId(userIdInput);
+    // setUserId(userIdInput);
+
   };
   return (
     <>
